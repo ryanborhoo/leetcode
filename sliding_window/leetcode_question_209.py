c20eadc620a92cc6f31843c49e0914f29ac2315f
Ryan Song <ryan.song@iterable.com>
2022-03-19 15:58:17 -0700

    Add question 209

--- sliding_window/leetcode_question_209.py
+++ sliding_window/leetcode_question_209.py
@@ -21,9 +21,15 @@ def minSubArrayLen(target, nums):
             total = total - nums[start]
             start += 1
         end += 1
-    return res if res != 'inf' else 0
+    return res if res != float('inf') else 0
 
 
-target = 7
-nums = [2, 3, 1, 2, 4, 3]
+# target = 7
+# nums = [2, 3, 1, 2, 4, 3]
+
+target = 4
+nums = [1, 4, 4]
+
+# target = 11
+# nums = [1, 1, 1, 1, 1, 1, 1, 1]
 print(minSubArrayLen(target, nums))
